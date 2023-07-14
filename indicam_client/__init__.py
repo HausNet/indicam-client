794d2928e610943369655be6687020964346efd4
HausNet Admin <admin@hausnet.io>
2023-07-14 16:16:15 -0400

    Initial checkin

---
+++ indicam_client/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for IndiCam Client."""
+
+__author__ = """Louis Calitz"""
+__email__ = 'louis@hausnet.io'
+__version__ = '0.1.0'
---
+++ tests/test_indicam_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `indicam_client` package."""
+
+
+import unittest
+
+from indicam_client import indicam_client
+
+
+class TestIndicam_client(unittest.TestCase):
+    """Tests for `indicam_client` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
